2655f3b3574c387af0ccfb27f513e145d03b2033
Raymond Cothern <rtcothern@gmail.com>
2014-11-08 17:48:51 -0800

    Changed page names

--- 1C/home.php
+++ 1C/home.php
@@ -13,8 +13,8 @@
                 <a href="addMovie.php"> Add a Movie</a> <br/>
                 <a href="addMoviePerson.php"> Associate a Movie with an Actor or Director</a> <br/>
             <h3>Browse existing data</h3>
-                <a href="showActor.php"> View information on an actor</a> <br/>
-                <a href="showMovie.php"> View information on a movie</a>  <br/>
+                <a href="viewActor.php"> View information on an actor</a> <br/>
+                <a href="viewMovie.php"> View information on a movie</a>  <br/>
             <h3>Perform a search</h3>
                 <a href="search.php"> Search for an Actor or a Director</a>  <br/>
             </center>
